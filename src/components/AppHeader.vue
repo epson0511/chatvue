c683917ef03eeb59fdc74aa23874f614193084ee
Jamie <jamielin@eztravel.com.tw>
2021-03-26 17:34:32 +0800

    註冊表單驗證邏輯

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -29,31 +29,32 @@
   >
     <div class="p-grid">
       <div class="p-field p-col-12">
-        <label for="nickname">暱稱</label>
+        <label for="nickname">暱稱 - 日後可變更</label>
         <InputText
           class="textbox"
           id="nickname"
           type="nickname"
           aria-describedby="nickname-help"
+          v-model.trim="nickname"
           autofocus
+          @focus="this.err.nickname = ''"
         />
+        <span id="nickname-help" class="p-error err-hander">
+          {{ err.nickname }}
+        </span>
       </div>
       <div class="p-field p-col-12">
-        <label for="email">信箱</label>
+        <label for="email">信箱 - 將設定為登入帳號</label>
         <InputText
           class="textbox"
           id="email"
           type="email"
-          aria-describedby="email-help"
+          aria-describedby="email-help err-hander"
           @blur="emailChecker"
+          @focus="this.err.email = ''"
           v-model.trim="email"
         />
-        <small
-          v-bind:style="{ visibility: visibility }"
-          id="email-help"
-          class="p-error"
-          >信箱已被使用</small
-        >
+        <span id="email-help" class="p-error err-hander">{{ err.email }}</span>
       </div>
       <div class="p-field p-col-12">
         <label for="password">密碼</label>
@@ -62,7 +63,12 @@
           id="password"
           type="password"
           aria-describedby="password-help"
+          v-model.trim="password"
+          @focus="(this.err.password = ''), (this.err.passwordconfirm = '')"
         />
+        <span id="password-help" class="p-error err-hander">{{
+          err.password
+        }}</span>
       </div>
       <div class="p-field p-col-12">
         <label for="passwordcheck">確認密碼</label>
@@ -72,7 +78,12 @@
           id="passwordcheck"
           type="password"
           aria-describedby="passwordcheck-help"
+          v-model.trim="passwordconfirm"
+          @focus="(this.err.password = ''), (this.err.passwordconfirm = '')"
         />
+        <span id="passwordcheck-help" class="p-error err-hander">{{
+          err.passwordconfirm
+        }}</span>
       </div>
     </div>
     <template #footer>
@@ -82,7 +93,7 @@
         @click="closeModalSignin"
         class="p-button-text"
       />
-      <Button label="確認" icon="pi pi-check" @click="closeModalSignin" />
+      <Button label="確認" icon="pi pi-check" @click="finalcheck" />
     </template>
   </Dialog>
 
@@ -96,15 +107,19 @@
   >
     <div class="p-grid">
       <div class="p-field p-col-12">
-        <label for="usernamelogin">帳號(電子信箱)</label>
+        <label for="usernamelogin">帳號(您的電子信箱)</label>
         <div class="textbox">
           <InputText
             id="usernamelogin"
             type="username"
             aria-describedby="usernamelogin-help"
             autofocus
-            style="width:100%"
+            style="width: 100%"
+            v-model.trim="usernamelogin"
           />
+          <span id="usernamelogin-help" class="p-error err-hander">{{
+            err.usernamelogin
+          }}</span>
         </div>
       </div>
       <div class="p-field p-col-12">
@@ -114,8 +129,12 @@
             id="passwordlogin"
             type="password"
             aria-describedby="passwordlogin-help"
-            style="width:100%"
+            style="width: 100%"
+            v-model.trim="passwordlogin"
           />
+          <span id="passwordlogin-help" class="p-error err-hander">{{
+            err.passwordlogin
+          }}</span>
         </div>
       </div>
     </div>
@@ -126,7 +145,7 @@
         @click="closeModalLogin"
         class="p-button-text"
       />
-      <Button label="確認" icon="pi pi-check" @click="closeModalLogin" />
+      <Button label="確認" icon="pi pi-check" @click="dologin" />
     </template>
   </Dialog>
 </template>
@@ -135,6 +154,7 @@ import Button from "primevue/button";
 import Dialog from "primevue/dialog";
 import Password from "primevue/password";
 import InputText from "primevue/inputtext";
+import { validateEmail } from "../utils/tools.js";
 import axios from "axios";
 
 export default {
@@ -144,7 +164,19 @@ export default {
       value1: null,
       displaySignin: false,
       email: "",
-      emailused: "hidden",
+      nickname: "",
+      password: "",
+      passwordconfirm: "",
+      usernamelogin: "",
+      passwordlogin: "",
+      err: {
+        nickname: "",
+        email: "",
+        password: "",
+        passwordconfirm: "",
+        usernamelogin: "",
+        passwordlogin: "",
+      },
     };
   },
   computed: {
@@ -155,9 +187,6 @@ export default {
       set(val) {
         this.$store.commit("statecenter/setLogin", val);
       },
-    },
-    visibility: function () {
-      return this.emailused;
     },
   },
   // watch: {
@@ -171,31 +200,103 @@ export default {
     },
     closeModalSignin() {
       this.displaySignin = false;
+      this.nickname = "";
+      this.email = "";
+      this.password = "";
+      this.passwordconfirm = "";
+      this.err.nickname = "";
+      this.err.email = "";
+      this.err.password = "";
+      this.err.passwordconfirm = "";
     },
     openModalLogin() {
       this.$store.commit("statecenter/setLogin", true);
     },
     closeModalLogin() {
       this.$store.commit("statecenter/setLogin", false);
+      this.usernamelogin = "";
+      this.passwordlogin = "";
+      this.err.usernamelogin = "";
+      this.err.passwordlogin = "";
     },
-    emailChecker() {
-      console.log(this.email);
+    async finalcheck() {
+      let count = 0;
+      const verifiedemail = validateEmail(this.email);
+
+      if (this.nickname === "") {
+        this.err.nickname = "請輸入暱稱";
+        count++;
+      }
+      if (this.email == "" || verifiedemail == false) {
+        this.err.email = "請輸入有效信箱";
+        count++;
+      }
+      if (this.password === "") {
+        this.err.password = "請輸入密碼";
+        count++;
+      }
+      if (this.passwordconfirm === "") {
+        this.err.passwordconfirm = "請輸入密碼";
+        count++;
+      }
+      if (this.passwordconfirm !== this.password) {
+        this.err.password = "密碼不一致";
+        this.err.passwordconfirm = "密碼不一致";
+        count++;
+      }
+      await this.emailChecker();
+      if (count == 0) {
+        this.doSignin();
+      }
+    },
+    async emailChecker() {
+      const { data } = await axios.get(
+        "http://localhost:8082/chat/rest/v1/userAPI/getUserByEmailForCheck?email=" +
+          this.email
+      );
+
+      if (data == null) {
+        if (validateEmail(this.email)) {
+          this.err.email = "";
+        } else {
+          this.err.email = "請輸入有效信箱";
+        }
+      } else {
+        this.err.email = "信箱已被使用";
+        console.log(data);
+      }
+    },
+    doSignin() {
+      let params = {
+        name: this.nickname,
+        email: this.email,
+        password: this.password,
+      };
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "",
+        "Access-Control-Allow-Origin": "*",
+      };
       axios
-        .get(
-          "http://localhost:8082/chat/rest/v1/userAPI/getUserByEmailForCheck?email=" +
-            this.email
-        )
-        .then((res) => {
-          if (res.data == null) {
-            this.emailused = "hidden";
-            console.log("無此帳號");
-          } else {
-            this.emailused = "visible";
-            console.log(res.data);
+        .post(
+          "http://localhost:8082/chat/rest/v1/userAPI/doSignIn",
+          JSON.stringify(params),
+          {
+            headers: headers,
           }
-        });
+        )
+        .then((res) => console.log(res.data));
+    },
+    dologin() {
+      if (this.usernamelogin === "") {
+        this.err.usernamelogin = "請輸入註冊信箱";
+      }
+      if (this.passwordlogin === "") {
+        this.err.passwordlogin = "請輸入密碼";
+      }
     },
   },
+
   components: {
     Button,
     Dialog,
@@ -229,9 +330,12 @@ export default {
 .textbox {
   width: 100%;
 }
-#email-help {
-  float: right;
-  visibility: hidden;
+.err-hander {
+  float: left;
+  font-size: 5px;
+  height: 16px;
+  width: 100%;
+  // visibility: hidden;
 }
 #nav {
   padding: 30px;
--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,15 +1,19 @@
-
 export const uuid = () => {
-    
-        var d = Date.now();
-        if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
-          d += performance.now(); //use high-precision timer if available
-        }
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-          var r = (d + Math.random() * 16) % 16 | 0;
-          d = Math.floor(d / 16);
-            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-        });
+  var d = Date.now();
+  if (
+    typeof performance !== "undefined" &&
+    typeof performance.now === "function"
+  ) {
+    d += performance.now(); //use high-precision timer if available
+  }
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+};
 
-}
-
+export const validateEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
